0083345c5b7a53cba4b9a43409edc4ad1eee2fcd
mcatorbenson <57915491+mcatorbenson@users.noreply.github.com>
2025-12-26 10:35:32 +0100

    Update lightbox.js

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const overlay = document.getElementById("lightbox-overlay");
   const overlayImg = document.getElementById("lightbox-image");
+  const overlayCaption = document.getElementById("lightbox-caption");
 
   document.querySelectorAll("a.lightbox").forEach(link => {
     link.addEventListener("click", e => {
       e.preventDefault();
+
+      const img = link.querySelector("img");
+
       overlayImg.src = link.href;
+      overlayImg.alt = img.alt;
+      overlayCaption.textContent = img.alt;
+
       overlay.style.display = "flex";
     });
   });
@@ -13,5 +20,6 @@ document.addEventListener("DOMContentLoaded", () => {
   overlay.addEventListener("click", () => {
     overlay.style.display = "none";
     overlayImg.src = "";
+    overlayCaption.textContent = "";
   });
 });
